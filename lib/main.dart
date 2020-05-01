f501edb18b252ee506af819102a0f78e3e038fc1
Valera <gavrilow777@gmail.com>
2020-05-01 18:29:31 +0300

    add stretch buttons

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,72 @@ class MainScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
         appBar: AppBar(
+          backgroundColor: Colors.black54,
           title: Text('Music application'),
         ),
         body: Center(
-          child: Row(children: <Widget>[
-            Text('This is body text for demo'),
-            FlatButton(
-              child: Text('play'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  AudioCache player = new AudioCache();
+                   player.play('note1.wav');
+                },
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  AudioCache player = new AudioCache();
+                  player.play('note2.wav');
+                },
+              ),
+            ),
+            Expanded(child: FlatButton(
+              color: Colors.yellowAccent,
               onPressed: () {
                 AudioCache player = new AudioCache();
-                 player.play('note1.wav');
+                player.play('note3.wav');
               },
-            )
+            ),),
+            Expanded(
+              child: FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  AudioCache player = new AudioCache();
+                  player.play('note4.wav');
+                },
+              ),
+            ),
+            Expanded(child: FlatButton(
+              color: Colors.blue,
+              onPressed: () {
+                AudioCache player = new AudioCache();
+                player.play('note5.wav');
+              },
+            ),),
+            Expanded(
+              child: FlatButton(
+                color: Colors.pinkAccent,
+                onPressed: () {
+                  AudioCache player = new AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+            ),
+            Expanded(child: FlatButton(
+              color: Colors.purple,
+              onPressed: () {
+                AudioCache player = new AudioCache();
+                player.play('note7.wav');
+              },
+            ),),
           ],),
         ),
       ),
